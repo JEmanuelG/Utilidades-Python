9fcf83e68c3a0183dd4cb0b37ea072e8223b02cf
Emanuel Guaráz <107538602+JEmanuelG@users.noreply.github.com>
2024-09-08 21:37:28 -0300

    Update excel.py

--- pandas/excel.py
+++ pandas/excel.py
@@ -3,10 +3,10 @@
 import pandas as pd
 
 # LEE EL FICHERO EXCEL
-fichero_excel = pd.ExcelFile('C:/Users/Emanuel/Desktop/Cursos/Python/Python/campeones del mundo.xlsx')
+fichero_excel = pd.ExcelFile('C:/Users/Emanuel/Desktop/Python/campeones del mundo.xlsx') # Acá va la dirección de la ubicación del archivo
 
 # LEE FICHERO CSV, QUE ES SIMILAR A EXCEL. Y DE ESTA MANERA YA QUEDA COMO UN DATAFRAME
-file_csv = pd.read_csv('C:/Users/Emanuel/Desktop/Cursos/Python/otros ejercicios/seccion21/poblacion.csv')
+file_csv = pd.read_csv('C:/Users/Emanuel/Desktop/Python/otros ejercicios/seccion21/poblacion.csv') # Acá va la dirección de la ubicación del archivo
 
 # CREA DATAFRAME CON LOS DATOS DE LA "Hoja1"
 dataframe = fichero_excel.parse('Hoja1')
